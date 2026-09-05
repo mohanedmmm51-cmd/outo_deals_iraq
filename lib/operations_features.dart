3bf88579e25140f9a96e8eed646b2d87c29d149e
mohanedmmm51-cmd <mohanedmmm51@gmail.com>
2026-09-06 00:46:46 +0300

    جهز عرض QR الآمن من تفاصيل الطلب

--- lib/operations_features.dart
+++ lib/operations_features.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
